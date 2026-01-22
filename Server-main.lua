9c666317973a82cfd82f808a60c6b9b9c4e52d3a
Redcraftcore <245560243+Redcraftcore@users.noreply.github.com>
2026-01-22 16:41:12 +0100

    V2
    Start of impimentation of new sys

---
+++ Server-main.lua
@@ -0,0 +1,33 @@
+--Server v2
+local modem = peripheral.wrap("top")
+local disk = perhibial.wrap("bottom")
+--Name settings
+local Servername = "test"
+local domain = ".com"
+--host and log for host start
+rednet.host("online",tostring(Servername..domain))
+term.clear()
+print("server hosted under"..tostring(Servername..domain))                  --log stamp
+
+--main handle loop
+local function handle()
+    while true do
+        local Cid , rmsg = rednet.receive()
+
+        --region clarify redirect
+        if rmsg.page == nil then
+            print("Computer"..Cid.." requested Home")                       --log stamp
+            fs.open("/disk/home","r")
+        else
+            print("Computer"..Cid.." requested "..tostring(rmsg.page))      --log stamp
+            local cmsg = fs.open(tostring(rmsg.page),"r")
+            rednet.send(Cid,cmsg)
+        end
+        --request type check(not yet implemented)
+        if rmsg.type == "req" then
+            print("Computer"..Cid.." requested page")                       --log stamp
+        elseif rmsg == "rel" then
+            print("Computer"..Cid.." requested reload of page")             --log stamp
+        end
+    end
+end
